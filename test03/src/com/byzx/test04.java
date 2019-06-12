f4b856f1fea924c35cf1348f5be972331e2de035
liping1314 <147511823@qq.com>
2019-06-12 16:50:29 +0800

    第一次提交

---
+++ test03/src/com/byzx/test04.java
@@ -0,0 +1,19 @@
+package com.byzx;
+/**@文件名: test04.java
+ * @类功能说明: 
+ * @作者: LiPing
+ * @Email: 147511823@qq.com
+ * @日期: 2019年6月12日下午2:39:42
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: LiPing</li> 
+ * 	 <li>日期: 2019年6月12日下午2:39:42</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class test04 {
+
+	public static void main(String[] args) {
+		System.out.println("你好");
+	}
+}
